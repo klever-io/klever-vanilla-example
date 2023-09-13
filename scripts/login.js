434d92dab8d3b08ac7995bb19991b37f8a7a8590
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-13 13:13:15 -0300

    feat: :sparkles: add home page and login

---
+++ scripts/login.js
@@ -0,0 +1,42 @@
+import { toast } from "./components/toast.js";
+import { signMessage } from "./config/index.js";
+
+const form = document.querySelector("#sign-in-form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const button = document.querySelector("#sign-in-button");
+  button.disabled = true;
+
+  try {
+    if (typeof window === "undefined" || !window.kleverWeb) {
+      throw Error("Klever Extension not found");
+    }
+
+    await window.kleverWeb.initialize();
+    const address = window.kleverWeb.getWalletAddress();
+    if (!address) {
+      throw Error("Cannot retrieve wallet address");
+    }
+
+    const message = await window.kleverWeb.signMessage(signMessage);
+    const result = await window.kleverWeb.validateSignature(
+      signMessage,
+      message,
+      address
+    );
+
+    if (!result) {
+      throw Error("Invalid signature");
+    }
+
+    window.location.replace("/private");
+  } catch (error) {
+    toast({
+      variant: "destructive",
+      title: error,
+    });
+  }
+
+  button.disabled = false;
+});
